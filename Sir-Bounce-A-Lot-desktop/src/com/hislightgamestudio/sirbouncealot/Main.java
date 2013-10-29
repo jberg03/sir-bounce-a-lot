29fdbd0bcdf4064535409a14f50c78d2aadb1263
John Berg <21scout0311@gmail.com>
2013-10-29 16:09:56 -0400

    fixed issues with the texture atlas file

--- Sir-Bounce-A-Lot-desktop/src/com/hislightgamestudio/sirbouncealot/Main.java
+++ Sir-Bounce-A-Lot-desktop/src/com/hislightgamestudio/sirbouncealot/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = SirBounceALot.TITLE;
-		cfg.useGL20 = false;
+		cfg.useGL20 = true;
 		cfg.width = 1280;
 		cfg.height = 720;
 		
