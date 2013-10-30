752c512c6bfa7ec01b570a0d1ffd023adb7f70f6
John Berg <21scout0311@gmail.com>
2013-10-30 11:55:12 -0400

    adding some more code

--- Sir-Bounce-A-Lot/src/com/hislightgamestudio/sirbouncealot/screens/MainMenu.java
+++ Sir-Bounce-A-Lot/src/com/hislightgamestudio/sirbouncealot/screens/MainMenu.java
@@ -2,4 +2,27 @@ package com.hislightgamestudio.sirbouncealot.screens;
 
 public class MainMenu extends AbstractScreen {
 
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		super.resize(width, height);
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		super.show();
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		super.dispose();
+	}
+	
 }
