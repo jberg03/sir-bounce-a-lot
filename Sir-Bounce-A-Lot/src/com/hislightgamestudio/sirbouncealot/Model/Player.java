78dacede0a1d12134fc813c4871bfc419e969447
John Berg <21scout0311@gmail.com>
2013-11-01 16:15:33 -0400

    added some empty collision functions

--- Sir-Bounce-A-Lot/src/com/hislightgamestudio/sirbouncealot/Model/Player.java
+++ Sir-Bounce-A-Lot/src/com/hislightgamestudio/sirbouncealot/Model/Player.java
@@ -5,11 +5,17 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactFilter;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.World;
+import com.hislightgamestudio.sirbouncealot.control.InputController;
 
-public class Player {
+public class Player extends InputController implements ContactFilter, ContactListener{
 	private Body body;
 	private Fixture fixture;
 	public final float width, height;
@@ -41,4 +47,36 @@ public class Player {
 	public void Update(){
 		body.applyForceToCenter(velocity, true);
 	}
+
+	@Override
+	public void beginContact(Contact contact) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endContact(Contact contact) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void preSolve(Contact contact, Manifold oldManifold) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postSolve(Contact contact, ContactImpulse impulse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean shouldCollide(Fixture fixtureA, Fixture fixtureB) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
 }
